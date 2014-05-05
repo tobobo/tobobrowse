1e78bbde1d0973d1385ec276572a027cc3940e23
Tobias Butler <tobiasab@gmail.com>
2014-05-05 01:11:38 -0700

    rearrange a little

--- tobobrowse.py
+++ tobobrowse.py
@@ -5,6 +5,7 @@ import ConfigParser
 
 config = ConfigParser.ConfigParser()
 
+
 if len(config.read('config')) < 1:
   print 'No config file.'
   exit(1)
@@ -16,23 +17,33 @@ class StripPathMiddleware(object):
     e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
     return self.app(e,h)
 
-def check(user, passwd):
-  if user == config.get('transmission', 'user') and passwd == config.get('transmission', 'pass'):
-    return True
-  return False
 
-@route('/')
-@auth_basic(check)
-def index():
-  return 'hello, friend!'
 
-@route('/torrents')
-@auth_basic(check)
-def torrents():
-  t = Transmission('localhost', 30446, '/transmission/rpc', config.get('transmission', 'user'), config.get('transmission', 'pass'))
-  return json.dumps({'torrents': t.get_torrent_list([])})
 
 def serve():
+
+  t = Transmission('localhost', 30446, '/transmission/rpc', config.get('transmission', 'user'), config.get('transmission', 'pass'))
+  
+  def check(user, passwd):
+    if user == config.get('transmission', 'user') and passwd == config.get('transmission', 'pass'):
+      return True
+    return False
+
+  @route('/')
+  @auth_basic(check)
+  def index():
+    return 'hello, friend!'
+
+  @route('/torrents')
+  @auth_basic(check)
+  def torrents():
+    return json.dumps({'torrents': t.get_torrent_list([])})
+
+  @route('/torrents/<name>')
+  @auth_basic(check)
+  def torrent(name):
+    return name
+
   tobobrowse = app()
   tobobrowse = StripPathMiddleware(tobobrowse)
   run(host='chips.whatbox.ca', port=8000, app=tobobrowse)

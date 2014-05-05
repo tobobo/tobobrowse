2b8947946ecdc5bdfb391c6479b89f4b7390dae5
Tobias Butler <tobiasab@gmail.com>
2014-05-04 22:24:16 -0700

    index

--- index.py
+++ index.py
@@ -1,6 +1,14 @@
-from bottle import route, run, auth_basic
+from bottle import app, route, run, auth_basic
 from daemonize import Daemonize
 from transmission import *
+import json
+
+class StripPathMiddleware(object):
+  def __init__(self, app):
+    self.app = app
+  def __call__(self, e, h):
+    e['PATH_INFO'] = e['PATH_INFO'].rstrip('/')
+    return self.app(e,h)
 
 def check(user, passwd):
   if user == 'tobobo':
@@ -15,11 +23,13 @@ def index():
 @route('/torrents')
 @auth_basic(check)
 def torrents():
-  t = Transmission('localhost', 30446, '/transmission/rpc', 'tobobo')
-  return t.get_torrent_list([])
+  t = Transmission('localhost', 30446, '/transmission/rpc', 'tobobo', '')
+  return json.dumps({'torrents': t.get_torrent_list([])})
 
 def main():
-  run(host='chips.whatbox.ca', port=8000)
+  tobobrowse = bottle.app()
+  tobobrowse = StripPathMiddleware(tobobrowse)
+  run(host='chips.whatbox.ca', port=8000, app=tobobrowse)
 
 daemon = Daemonize(app='tobobrowse', pid='/tmp/tobobrowse.pid', action=main)
 daemon.start()

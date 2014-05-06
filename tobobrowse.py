9390ddd336c9c0b80ea5cabd81a94e0de58b8e37
Tobias Butler <tobiasab@gmail.com>
2014-05-05 17:32:46 -0700

    parens?

--- tobobrowse.py
+++ tobobrowse.py
@@ -34,15 +34,13 @@ class EnableCors(object):
 
   def apply(self, fn, context):
     def _enable_cors(*args, **kwargs):
-      # set CORS headers
       response.headers['Access-Control-Allow-Origin'] = '*'
-      response.headers['Access-Control-Allow-Methods'] = \
-        'GET, POST, PUT, OPTIONS'
-      response.headers['Access-Control-Allow-Headers'] = \
-        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+      (response.headers['Access-Control-Allow-Methods'] = 
+        'GET, POST, PUT, OPTIONS')
+      (response.headers['Access-Control-Allow-Headers'] = 
+        'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token')
 
       if request.method != 'OPTIONS':
-        # actual request; reply with the actual response
         return fn(*args, **kwargs)
 
     return _enable_cors

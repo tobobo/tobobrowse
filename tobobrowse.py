08641e628ba3a8b6775a9def78c8548f577d0bf6
Tobias Butler <tobiasab@gmail.com>
2014-05-05 17:27:52 -0700

    back slashin

--- tobobrowse.py
+++ tobobrowse.py
@@ -36,9 +36,9 @@ class EnableCors(object):
     def _enable_cors(*args, **kwargs):
       # set CORS headers
       response.headers['Access-Control-Allow-Origin'] = '*'
-      response.headers['Access-Control-Allow-Methods'] = 
+      response.headers['Access-Control-Allow-Methods'] = \
         'GET, POST, PUT, OPTIONS'
-      response.headers['Access-Control-Allow-Headers'] = 
+      response.headers['Access-Control-Allow-Headers'] = \
         'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
       if request.method != 'OPTIONS':

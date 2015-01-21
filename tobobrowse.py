7a04d9ae67e694d2e35a7e58bbcf4c5936625d60
Tobias Butler <tobiasab@gmail.com>
2015-01-21 00:51:57 -0800

    whack headers

--- tobobrowse.py
+++ tobobrowse.py
@@ -278,7 +278,7 @@ def serve():
         file_handler.seek(file_offset)
       else:
         file_offset = 0
-      response.set_header('Content-Range', 'bytes %i-%i/%i'.format(file_offset, file_size, file_size))
+      response.set_header('Content-Range', 'bytes {!i}-{!i}/{!i}'.format(file_offset, file_size, file_size))
       while True:
         data = file.read(file_handler, 8388608)
         if not data:
